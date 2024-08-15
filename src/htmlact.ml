dbuenzli/htmlact

commit 9c6d78de3132545f1fd50ed0f162234b8587d5c0

Rename to htmlact.

src/htmlact.ml
@@ -1,5 +1,5 @@
 (*---------------------------------------------------------------------------
-   Copyright (c) 2021 The hc programmers. All rights reserved.
+   Copyright (c) 2021 The htmlact programmers. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
   ---------------------------------------------------------------------------*)
 
@@ -63,16 +63,16 @@ end
 
 (* Headers *)
 
-let hc = Http.Headers.name "hc"
-let redirect = Http.Headers.name "hc-redirect"
-let reload = Http.Headers.name "hc-reload"
-let location_push = Http.Headers.name "hc-location-push"
-let location_replace = Http.Headers.name "hc-location-replace"
-let location_title = Http.Headers.name "hc-location-title"
+let htmlact = Http.Headers.name "htmlact"
+let redirect = Http.Headers.name "htmlact-redirect"
+let reload = Http.Headers.name "htmlact-reload"
+let location_push = Http.Headers.name "htmlact-location-push"
+let location_replace = Http.Headers.name "htmlact-location-replace"
+let location_title = Http.Headers.name "htamlact-location-title"
 let encode_location_title = Http.Pct.encode `Uri
 
 (*---------------------------------------------------------------------------
-   Copyright (c) 2021 The hc programmers
+   Copyright (c) 2021 The htmlact programmers
 
    Permission to use, copy, modify, and/or distribute this software for any
    purpose with or without fee is hereby granted, provided that the above

.merlin
@@ -1,4 +1,4 @@
-PKG b0.kit b0 brr webs webs.kit webs.httpc webs.html webs.unix
+PKG b0.kit b0 brr webs webs.kit webs.cli webs.unix htmlit
 S src
 S examples
 B _b0/b/**

B0.ml
@@ -9,26 +9,27 @@ let webs_cli = B0_ocaml.libname "webs.cli"
 let webs_unix = B0_ocaml.libname "webs.unix"
 let htmlit = B0_ocaml.libname "htmlit"
 
-let hc = B0_ocaml.libname "hc"
-let hc_page = B0_ocaml.libname "hc.page"
+let htmlact = B0_ocaml.libname "htmlact"
+let htmlact_page = B0_ocaml.libname "htmlact.page"
 
 (* Libraries *)
 
-let hc_lib =
-  let srcs = Fpath.[ `File (v "src/hc.mli"); `File (v "src/hc.ml") ] in
+let htmlact_lib =
+  let srcs = [ `Dir (Fpath.v "src") ] in
   let requires = [webs; htmlit] in
-  B0_ocaml.lib hc ~doc:"Hc library" ~srcs ~requires
+  B0_ocaml.lib htmlact ~doc:"htmlact library" ~srcs ~requires
 
-let hc_page_lib =
-  let srcs = Fpath.[ `File (v "src/hc_page.mli"); `File (v "src/hc_page.ml")] in
+let htmlact_page_lib =
+  let srcs = [ `Dir (Fpath.v "src/page")] in
   let requires = [brr] in
-  B0_ocaml.lib hc_page ~doc:"Web page driver library" ~srcs ~requires
+  B0_ocaml.lib htmlact_page ~doc:"Webpage driver library" ~srcs ~requires
 
-let hc_page_js =
-  let srcs = Fpath.[ `File (v "src/hc_page_init.ml"); ] in
-  let requires = [hc_page] in
+let htmlact_page_js =
+  let srcs = [`Dir (Fpath.v "src/standalone")] in
+  let requires = [htmlact_page] in
   let meta = B0_jsoo.meta ~requires () in
-  B0_jsoo.exe "hc-page.js" ~meta ~srcs
+  let doc = "Self-contained webpage driver" in
+  B0_jsoo.exe "htmlact-page.js" ~doc ~meta ~srcs
 
 (* Examples *)
 
@@ -48,14 +49,14 @@ let serve_reload b u ~args = match B0_unit.get_meta B0_meta.exe_file u with
     let code = match st with `Exited c -> c | `Signaled c -> 128 + c in
     Ok (Os.Exit.code code)
 
-let hc_examples =
-  let doc = "Hc examples" in
-  let requires = [ hc; webs; webs_cli; webs_unix; htmlit] in
+let htmlact_examples =
+  let doc = "Htmlact examples" in
+  let requires = [ htmlact; webs; webs_cli; webs_unix; htmlit] in
   let srcs = Fpath.[ `Dir (v "examples"); ] in
   let meta = B0_meta.(empty |> add B0_ocaml.Meta.supported_code `Native) in
-  let wrap proc b = B0_build.require b hc_page_js; proc b in
+  let wrap proc b = B0_build.require b htmlact_page_js; proc b in
   let action = serve_reload in
-  B0_ocaml.exe "hc-examples" ~wrap ~doc ~srcs ~requires ~action ~meta
+  B0_ocaml.exe "htmlact-examples" ~wrap ~doc ~srcs ~requires ~action ~meta
 
 (* Packs *)
 
@@ -63,28 +64,29 @@ let default =
   let meta =
     let open B0_meta in
     empty
-    |> add authors ["The hc programmers"]
+    |> add authors ["The htmlact programmers"]
     |> add maintainers ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
-    |> add homepage "https://erratique.ch/software/hc"
-    |> add online_doc "https://erratique.ch/software/hc/doc"
+    |> add homepage "https://erratique.ch/software/htmlact"
+    |> add online_doc "https://erratique.ch/software/htmlact/doc"
     |> add licenses ["ISC"]
-    |> add repo "git+https://erratique.ch/repos/hc.git"
-    |> add issues "https://github.com/dbuenzli/hc/issues"
-    |> add description_tags ["front-end"; "ui"; "html"; "org:erratique"; ]
+    |> add repo "git+https://erratique.ch/repos/htmlact.git"
+    |> add issues "https://github.com/dbuenzli/htmlact/issues"
+    |> add description_tags
+      ["front-end"; "ui"; "html"; "web"; "org:erratique"; ]
     |> add B0_opam.Meta.build
       {|[["ocaml" "pkg/pkg.ml" "build" "--dev-pkg" "%{dev}%"
-           "--with-webs" "%{webs:installed}%"]]|}
+           "--with-webs" "%{webs:installed}%"
+           "--with-htmlit" "%{htmlit:installed}%"]]|}
     |> add B0_opam.Meta.depends [
       "ocaml", {|>= "4.08.0"|};
       "ocamlfind", {|build|};
       "ocamlbuild", {|build|};
       "topkg", {|build & >= "1.0.3"|};
       "brr", "";
-      "webs", "";
-      "htmlit", "";
       "js_of_ocaml-compiler", {|>= "3.7.1"|};
     ]
+    |> add B0_opam.Meta.depopts [ "webs", ""; "htmlit", ""]
     |> tag B0_opam.tag
   in
-  B0_pack.v "default" ~doc:"hc package" ~meta ~locked:true @@
+  B0_pack.v "default" ~doc:"htmlact package" ~meta ~locked:true @@
   B0_unit.list ()

BRZO
@@ -1,0 +1,1 @@
+(srcs-x pkg test examples)

_tags
@@ -1,8 +1,13 @@
 true : bin_annot, safe_string
 
 <_b0> : -traverse
+
 <src> : include
-<src/hc.*> : package(webs htmlit)
-<src/hc_page.*> : package(brr)
-<src/hc_page_init.*> : no_check_prims, package(brr)
+<src/htmlact.*> : package(webs htmlit)
+
+<src/page> : include
+<src/page/htmlact_page.*> : package(brr)
+
+<src/standalone> : include
+<src/standalone/htmlact_page_init.*> : no_check_prims, package(brr)
 <examples/*> : thread, package(webs webs.unix webs.cli htmlit)

doc/index.mld
@@ -1,23 +1,24 @@
-{0 Hc {%html: <span class="version">%%VERSION%%</span>%}}
+{0 Htmlact {%html: <span class="version">%%VERSION%%</span>%}}
 
-Hc declares dynamic web pages by using HTML data attributes to
+Htmlact declares dynamic webpages by using HTML data attributes to
 seamlessly update the page with HTML fragments requested from the
 server.
 
-Using hypertext as the interaction substrate keeps logic on the server
+Using HTML as the interaction substrate keeps logic on the server
 and, in the simplest cases, reduces front-end client code to custom
-CSS animations and an invocation of the {!Hc_page.init} function.
+CSS animations and an invocation of the {!Htmlact_page.init} function
+or adding the [htmlact-page.js] script to your page.
 
-[hc] was inspired by {{:https://htmx.org/}htmx}.
+Htmlact was inspired by {{:https://htmx.org/}htmx}.
 
 {1:quick_start Quick start}
 
 If you really do not want to write a single line of front-end code,
-include the page [$(opam var share)/hc/hc-page.js] in your page. Now
+include the page [$(opam var share)/htmlact/htmlact-page.js] in your page. Now
 the following:
 
 {v
-<script type="text/javascript" defer src="hc-page.js"></script>
+<script type="text/javascript" defer src="htmlact-page.js"></script>
 <button data-request="POST /clicked"
         data-effect="element">Replace me</button>
 v}
@@ -25,31 +26,41 @@ v}
 instructs a click on the button to perform a POST request to
 [/clicked] and replace the button with the HTML response. That's it.
 
-If you installed [hc] with tests ([opam install -t hc]). The
-[hc-examples] binary serves a few interaction examples.
+If you installed [htmlact] with tests ([opam install -t htmlact]). The
+[htamlct-examples] binary serves a few interaction examples and links
+to their sources.
 
 {1:manuals Manuals}
 
 {ul
 {- The {{!page-manual}manual} explains it all.}}
 
-{1:api API}
+{1:library_htmlact Library [htmlact]}
+
+That's not strictly needed. Only if you use {!Webs} and {!Htmlit}.
 
 {!modules:
-Hc
-Hc_page
+Htmlact}
+
+{1:library_htmlact_page Library [htmlact.page]}
+
+That's not srictly needed either. You can instead add the [htmlact-page.js]
+script to your page.
+
+{!modules:
+Htmlact_page
 }
 
 {1:todo TODO}
 
 {ul
-{- Review animation interaction for hc-in, it's not so
+{- Review animation interaction for htmlact-in, it's not so
    good to have transition on bare elements which
-   are triggered according to .hc-in -> bare}
-{- Provide and trigger events for [Hc] behaviour itself, this
-   allows to chain [Hc] connections in interesting ways and
+   are triggered according to .htmlact-in -> bare}
+{- Provide and trigger events for [Htmlact] behaviour itself, this
+   allows to chain [Htmlact] connections in interesting ways and
    helps coordinating with other front end code.
-   {b Update.} Something was hastily added in {!Hc.Ev}.
+   {b Update.} Something was hastily added in {!Htmlact.Ev}.
    needs proper design and documentation.}
 {- Because of resource costs we likely want something like
    multi-target response updates for SSE and websockets. Also
@@ -77,7 +88,7 @@ Hc_page
    Provide local substitution.}
 {- We likely want to expose the dom effecting stuff (for now
    exposed hastily for hyperbib and undocumented), make a nice
-   api and let [hc] use it.}
+   api and let [htmlact] use it.}
 {- Allow to tweak the
    {{:https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy}
     [Referrer-Policy]} for requests. For now we use [same-origin]. Possibly

doc/manual.mld
@@ -2,7 +2,7 @@
 
 {1:request Request elements}
 
-The idea behind [Hc] is to allow any HTML element to perform requests
+The idea behind [htmlact] is to allow any HTML element to perform requests
 on the server for HTML fragments. These requests are specified via the
 {{!data_request}[data-request]} attribute. We call {e request
 elements}, elements that bear such an attribute.
@@ -27,7 +27,7 @@ Each request element defines a connection cycle in which a few other
 elements take part. All these other elements are always defined via
 attributes of the request element. They can all point to the same
 element. In particular if they are not specified they mostly default
-to the request element.
+to the request element itself.
 
 {ul
 {- The {e request} element. This is the element that has the
@@ -39,15 +39,15 @@ to the request element.
    [data-event]} attribute triggers the request.}
 {- The {e query} elements, specified by the {{!data_query}[data-query]}
    attribute. These are the element that are looked up to devise the
-   request's query data (in the request URL for [GET] and [HEAD], in
-   the body otherwise).}
+   request's query data (transmitted in the request URL for [GET] and [HEAD],
+   or in the body for other methods).}
 {- The {e target} element. This is the element specified by the
    {{!data_target}[data-target]} attribute. It specifies the
    relative location of the request response effect, that is where
    and how the HTML fragment from the server gets inserted in the page.}
 {- The {e feedback} element(s), specified by {{!data_feedback}[data-feedback]}.
    These are additional elements on which the connection cycle is feedback
-   using classes.}}
+   using CSS classes.}}
 
 Every request element on the page cycle through the following steps.
 
@@ -59,31 +59,31 @@ Every request element on the page cycle through the following steps.
    {{!data_feedback}feedback} {e element(s)}.}
 {- Perform the request with the {{:data_query}query}. Redirections
    are followed. The request and feedback elements get classified
-   as [hc-request]}
+   as [htmlact-request]}
 {- Wait for the response. If an error occurs classify
-   the request and feedback elements with [hc-error] and stop here.
-   Otherwise unclassify [hc-request] and proceed. }
+   the request and feedback elements with [htmlact-error] and stop here.
+   Otherwise unclassify [htmlact-request] and proceed. }
 {- Process any {{!response_headers}special header} in the response. This
    may stop the cycle here (e.g. on {{!header_reload}reload} or
    {{!header_redirect}redirect}).}
 {- Using the HTML fragment found in the server response, perform the
    {{:data_effect}effect} on the {{:data_target}target}. If elements
    are being removed from the page, they first get classified with
-   {{!class_hc_out}[hc-out]} for a while before being effectively removed.
-   Elements that are introduced get transiently classified with
-   {{!class_hc_in}[hc-in].}}
+   {{!class_htmlact_out}[htmlact-out]} for a while before being effectively
+   removed. Elements that are introduced get transiently classified with
+   {{!class_htmlact_in}[htmlact-in].}}
 {- If the request is:
    {ul
-   {- a websocket, loop to 1 with the same socket.}
-   {- SSE connection, loop to 4.}
+   {- A websocket, loop to 1 with the same socket.}
+   {- An SSE connection, loop to 4.}
    {- Otherwise loop to 1 (if the element still exists
-      after the effect)}}}}
+      after the effect).}}}}
 
 {1:css_class_feedback CSS class feedback}
 
 The following classes can be used to track the connection cycle.
 
-{2:class_hc_request [hc-request]}
+{2:class_htmlact_request [htmlact-request]}
 
 This class is applied on the {{!data_request}request element} and the
 {{!data_feedback}feedback element(s)} whenever a request to the server
@@ -96,56 +96,60 @@ is taking longer than 500ms.
 
 {v
 .spinner { visibility: hidden; }
-.hc-request .spinner { animation: spin 1s linear 500ms infinite; }
+.htmlact-request .spinner { animation: spin 1s linear 500ms infinite; }
 
 @keyframes spin
 { from { visibility: visible; } to { transform: rotate(360deg); }}
 v}
 
-{2:class_hc_error [hc-error]}
+{2:class_htmlact_error [htmlact-error]}
 
 This class is applied on the {{!data_request}request element} and
 the {{!data_feedback}feedback element} whenever a request failed.
-This includes if the request returns a 4XX or 5XX result.
+This includes if the request returns a 4XX or 5XX response.
 
-{2:class_hc_in [hc-in] and [hc-in-parent]}
+{2:class_htmlact_in [htmlact-in] and [htmlact-in-parent]}
 
 These classes are applied for a small amount of time (one render
-frame) when the {{!data_effect}effect} is performed. [hc-in] is
-applied on the root of HTML trees that are inserted in the DOM by the
-effect. [hc-in-parent] is applied on their parent.
+frame) when the {{!data_effect}effect} is performed:
+{ul
+{- [htmlact-in] is applied on the root of HTML trees that are inserted
+   in the DOM by the effect.}
+{- [htmlact-in-parent] is applied on their parent.}}
 
 These classes are used to trigger CSS animations. For example to fade
 in an element in 250ms on insertion use:
 
 {v
-.my-element.hc-in { opacity: 0; }
+.my-element.htmlact-in { opacity: 0; }
 .my-element { opacity: 1; transition: opacity 250ms; }
 v}
 
-{2:class_hc_out [hc-out] and [hc-out-parent]}
+{2:class_htmlact_out [htmlact-out] and [htmlact-out-parent]}
 
 These classes are applied on elements (if any) that are removed by the
-{{!data_effect}effect} just before it is performed.  [hc-out] is
-applied on elements that will be removed. [hc-out-parent] is on their
-parent.
+{{!data_effect}effect} just before it is performed:
+{ul
+{- [htmlact-out] is applied on elements that will be removed.}
+{- [htmlact-out-parent] is on their parent.}}
 
 To determine the amount of time during which the classes are applied
 before the effect is performed, first the classes are applied on the
 elements. Then for each of these elements we take the maximum value of
-either CSS attribute [--hc-out-duration], [animation-duration] or
+either CSS attribute [--htmlact-out-duration], [animation-duration] or
 [transition-duration]. The maximal value of all these durations defines
 the duration.
 
 If the elements animating are descendent of those that are classified
-by [hc-out] or [hc-out-parent], use the [--hc-out-duration] attribute
-on the [.hc-out] elements to set the time needed. For example the
-following ensures that the [hc-out] and [hc-out-parent] classes are
+by [htmlact-out] or [htmlact-out-parent], use the
+[--htmlact-out-duration] attribute on the [.htmlact-out] elements to set
+the time needed. For example the
+following ensures that the [htmlact-out] and [htmlact-out-parent] classes are
 maintained on the elements to be removed for 500ms before being
 effectively removed:
 
 {v
-.hc-out { --hc-out-duration: 500ms }
+.htmlact-out { --htmlact-out-duration: 500ms }
 v}
 
 {1:attref Attribute reference}
@@ -225,6 +229,7 @@ modifiers for the event:
    [false] the event did not occur.}}
 
 If the attribute is unspecified this is:
+
 {ul
 {- [submit] for [form] elements.}
 {- [change] for [input], [textarea] and [select].}
@@ -245,8 +250,8 @@ on which the {{!data_request}request} response performs its
 
 {2:data_effect [data-effect="<eff>"]}
 
-The value [<eff>] determines the way the HTML response is used on
-the {{!data_target}target}. This can be:
+The value [<eff>] determines the way the HTML response is used on the
+{{!data_target}target}. This can be:
 
 {ul
 {- [children] to replaces the children of the target (default).}
@@ -265,11 +270,12 @@ If unspecified this is [children].
 
 DOM elements in addition to {{!data_target}target element} on which
 the connection cycle is feedback with
-{{!class_hc_request}[hc-request]} and {{!class_hc_error}[hc-error]}.
+{{!class_htmlact_request}[htmlact-request]} and
+{{!class_htmlact_error}[htmlact-error]}.
 
 {1:selector Selector syntax}
 
-Attributes of [hc] use
+Attributes of [htmlact] use
 {{:https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors}CSS
 selectors} with
 {{:https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector}
@@ -320,42 +326,43 @@ The server should respond with an HTML fragment to be inserted with
 respect to the {{!data_target}request target}. Additionally it can use
 the headers described next to control the client.
 
-{2:header_redirect [hc-redirect: <url>]}
+{2:header_redirect [htmlact-redirect: <url>]}
 
-Redirects the page to [<url>]. The body of the response is ignored by [Hc].
+Redirects the page to [<url>]. The body of the response is ignored by
+[htmlact] page driver.
 
-{2:header_reload [hc-reload: true]}
+{2:header_reload [htmlact-reload: true]}
 
-Reloads the page. The body of the response is igored by [Hc].
+Reloads the page. The body of the response is igored by the [htmlact]
+page driver.
 
-{2:header_location_push [hc-location-push: <url>]}
+{2:header_location_push [htmlact-location-push: <url>]}
 
 Pushes location [<url>] on the history stack.
 
-{2:header_location_replace [hc-location-replace: <url>]}
+{2:header_location_replace [htmlact-location-replace: <url>]}
 
 Replaces current location by [<url>] (without reloading). Has no effect
-if {{!header_location_push}[hc-location-push]} is present.
+if {{!header_location_push}[htmlact-location-push]} is present.
 
-{2:header_location_title [hc-location-title: <pct-title>]}
+{2:header_location_title [htmlact-location-title: <pct-title>]}
 
-Replaces the current title by percent encoded title [<pct-title>].
-Mostly used in conjunction with {{!header_location_push}[hc-location-push]}
-or {{!header_location_replace}[hc-location-replace]}.
+Replaces the current title by the percent encoded title [<pct-title>].
+Mostly used in conjunction with {{!header_location_push}[htmlact-location-push]}
+or {{!header_location_replace}[htmlact-location-replace]}.
 
 {1:request_headers Request HTTP headers}
 
-{2:header_hc [hc: true]}
+{2:header_htmlact [htmlact: true]}
 
-Indicates the request is made by [Hc].
+Indicates the request is made by the [htamlact] page driver.
 
 {2:header_referer [referer: <url>]}
 
 This is the standard HTTP [referer] header, it indicates the URL of
-the page (without the fragment) that performs the request. [hc] makes
+the page (without the fragment) that performs the request. [htmlact] makes
 requests with the
-{{:https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy#directives}
-[same-origin]} policy.
+{{:https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy#directives}[same-origin]} policy.
 
 If your HTML fragment returns relative URLs they will be interpreted
 relative to [<url>] once they are injected in the DOM.

examples/autocomplete.ml
@@ -1,5 +1,5 @@
 (*---------------------------------------------------------------------------
-   Copyright (c) 2021 The hc programmers. All rights reserved.
+   Copyright (c) 2021 The htmlact programmers. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
   ---------------------------------------------------------------------------*)
 
@@ -22,7 +22,7 @@ let description = Example.description [ El.div [El.txt
 let style = {css|
 @keyframes spin { from { visibility: visible; opacity:0 } to { opacity:1 }}
 .spinner { visibility: hidden; }
-.spinner.hc-request
+.spinner.htmlact-request
 { animation: spin var(--dur-medium) ease var(--dur-short) infinite alternate; }
 |css}
 
@@ -47,9 +47,10 @@ let autocomplete_options r =
 let bookmark_name_input urlf =
   let list_id = "titles" in
   let input =
-    let r = Hc.request ~meth:`POST (Example.uf urlf "") in
-    let t = Hc.target (":up #" ^ list_id) and f = Hc.feedback ":up .spinner" in
-    let e = Hc.event ~debounce_ms:500 "input" in
+    let r = Htmlact.request ~meth:`POST (Example.uf urlf "") in
+    let t = Htmlact.target (":up #" ^ list_id) in
+    let f = Htmlact.feedback ":up .spinner" in
+    let e = Htmlact.event ~debounce_ms:500 "input" in
     let at =
       At.[ class' "field"; name "title"; type' "search"; int "size" 25;
            v "list" list_id; autocomplete "off"; autofocus;
@@ -80,7 +81,7 @@ let serve r = match Http.Request.path r with
 | p -> Http.Response.not_found_404 ()
 
 (*---------------------------------------------------------------------------
-   Copyright (c) 2021 The hc programmers
+   Copyright (c) 2021 The htmlact programmers
 
    Permission to use, copy, modify, and/or distribute this software for any
    purpose with or without fee is hereby granted, provided that the above

commit 75d090ddc3b9a54397bb2c1043b2021cc20023ef

Htmlact.location_title: fix typo introduced by 9c6d78de.

src/htmlact.ml
@@ -87,5 +87,5 @@ let redirect = Http.Headers.name "htmlact-redirect"
 let reload = Http.Headers.name "htmlact-reload"
 let location_push = Http.Headers.name "htmlact-location-push"
 let location_replace = Http.Headers.name "htmlact-location-replace"
-let location_title = Http.Headers.name "htamlact-location-title"
+let location_title = Http.Headers.name "htmlact-location-title"
 let encode_location_title = Http.Pct.encode `Uri